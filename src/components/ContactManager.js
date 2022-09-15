f216d420bc07d2871452b442dca2acb5ab2222c6
Prabhat Ranjan <prabhat@vinsol.com>
2022-09-15 20:36:15 +0530

    Added the search functionality

--- src/components/ContactManager.js
+++ src/components/ContactManager.js
@@ -1,23 +1,38 @@
 import React, { useState } from 'react';
 import Contact from './Contact';
 import ContactForm from './ContactForm';
+import Search from './Search';
 
 function ContactManager() {
   const [contactList, updateContactList] = useState([]);
+  const [searchText, setSearchText] = useState('');
+  const handleSearchInput = (value) => {
+    setSearchText(value);
+  };
   const removeContact = (contact) => {
     updateContactList(contactList.filter((contactVal) => contactVal.id !== contact.id));
   };
   const addContact = (contact) => {
-    console.log('Conatct', contact);
     updateContactList([...contactList, contact]);
+  };
+  const getContactListToRender = () => {
+    if (searchText === '') {
+      return contactList;
+    }
+    return contactList.filter((contact) => (contact.firstName.includes(searchText)
+    || (contact.lastName.includes(searchText)) || (
+      contact.email.includes(searchText))));
   };
   return (
     <div>
       <div>
+        <Search value={searchText} handleSearchInput={handleSearchInput} />
+      </div>
+      <div>
         <ContactForm addContact={addContact} />
       </div>
       <div className="ContactList">
-        {contactList.map((contact) => (
+        {getContactListToRender().map((contact) => (
           <Contact
             key={contact.id}
             contact={contact}
